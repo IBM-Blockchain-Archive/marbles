cb3e7fe151bfbdaf99654ac665456d5ed2dfce45
Ben Smith <benjsmi@us.ibm.com>
2016-01-19 13:09:15 -0500

    Forget it--just use npm.

--- node_modules/obc-js/node_modules/nodegit/build/Release/.deps/Release/nodegit.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/nodegit.node := rm -rf "Release/nodegit.node" && cp -af "Release/obj.target/nodegit.node" "Release/nodegit.node"
--- node_modules/obc-js/node_modules/nodegit/lib/config.js
+++
@@ -1,8 +0,0 @@
-var NodeGit = require("../");
-
-var Config = NodeGit.Config;
-
-// Backwards compatibility.
-Config.prototype.getString = function() {
-  return this.getStringBuf.apply(this, arguments);
-};
