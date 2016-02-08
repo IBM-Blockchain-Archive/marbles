938d0da15fd0e03a5ffbd442e9ea4666068e752e
David <dshuffma@us.ibm.com>
2016-02-08 16:53:01 -0500

    - remove dead func

--- public/js/simplestuff1.js
+++ public/js/simplestuff1.js
@@ -108,18 +108,6 @@ $(document).on('ready', function() {
 			showAdminPanel(true);
 		}
 	}
-	
-	//format datetime
-	function formatDate(date) {
-		var hours = date.getHours();
-		var minutes = date.getMinutes();
-		var ampm = hours >= 12 ? 'pm' : 'am';
-		hours = hours % 12;
-		hours = hours ? hours : 12; // the hour '0' should be '12'
-		minutes = minutes < 10 ? '0'+minutes : minutes;
-		var strTime = hours + ':' + minutes + ' ' + ampm;
-		return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
-	}
 });
 
 
