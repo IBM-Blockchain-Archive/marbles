bdac70a0e90cb992243c7ac9668194bec188c0f9
David <dshuffma@us.ibm.com>
2016-03-04 11:32:25 -0500

    - removed cci

--- routes/site_router.js
+++ routes/site_router.js
@@ -46,33 +46,4 @@ router.route("/p2/:page?").get(function(req, res){
 	res.render('part2', {title: 'Marbles Part 2', bag: {setup: setup, e: process.error}} );
 });
 
-
-
-// ============================================================================================================================
-// Chaincode Summary File List
-// ============================================================================================================================
-router.route("/cc/summary").get(function(req, res){
-	fs.readdir('./cc_summaries/', cb_got_names);											//get file names
-	function cb_got_names(err, obj){
-		res.status(200).json(obj);
-	}
-});
-
-// ============================================================================================================================
-// Chaincode Investigator
-// ============================================================================================================================
-router.route("/cci/:filename?").get(function(req, res){
-	var cc = {};
-	if(req.params.filename){
-		try{
-			console.log('loading cc summary:', req.params.filename);
-			cc = require('../cc_summaries/' + req.params.filename + '.json');
-		}
-		catch(e){
-			console.log('error loading chaincode summary file', e);
-		};
-	}
-	res.render('investigate', {title: 'Investigator', bag: {cc: cc, setup: setup}} );
-});
-
 module.exports = router;
