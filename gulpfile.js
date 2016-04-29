e1dc3727f01b87b2b29a36e004a44b3ff6173399
David <dshuffma@us.ibm.com>
2016-04-29 17:47:07 -0400

    - updated gulp file and cleaned uneeded dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,42 @@
-/* global templatizer */
 ///// Gulp Dependencies /////
-var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	concat = require('gulp-concat'),
-	minifyCss = require('gulp-minify-css'),
-	rename = require("gulp-rename"),
-	watch = require('gulp-watch'),
-	sourcemaps = require('gulp-sourcemaps'),
-	uglify = require('gulp-uglify'),
-	spawn = require('child_process').spawn,
-	node;
+var path = require('path');
+var gulp = require('gulp');
+var	sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
+var spawn = require('child_process').spawn;
+var node, env = {};
 
 ////// Build Tasks ///////
 gulp.task('build-sass', function () {
 	gulp.src('./src/scss/*.scss')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./src/scss/temp'))						//build them here first
-		.pipe(concat('main.css'))							//concat them all
-		.pipe(gulp.dest('./public/css'))
-		.pipe(minifyCss())										//minify
-		.pipe(rename("main.min.css"))
-		.pipe(gulp.dest('./public/css'));						//dump it here
+		.pipe(gulp.dest(path.join(__dirname,'/scss/temp')))			//build them here first
+		.pipe(concat('main.css'))									//concat them all
+		.pipe(gulp.dest(path.join(__dirname, '/public/css')))
+		.pipe(cleanCSS())											//minify
+		.pipe(rename('main.min.css'))
+		.pipe(gulp.dest(path.join(__dirname,'/public/css')));		//dump it here
 });
 
 ////// Run Server Task ///////
 gulp.task('server', function() {
-	if (node) node.kill();
-	node = spawn('node', ['app.js'], {stdio: 'inherit'});		//command, file, options
+	if(node) node.kill();
+	node = spawn('node', ['app.js'], {env: env, stdio: 'inherit'});	//command, file, options
 });
-
 
 ////// Watch Tasks //////
 gulp.task('watch-sass', ['build-sass'], function () {
-	gulp.watch('./src/scss/*.scss', ['build-sass']);
+	gulp.watch(path.join(__dirname, '/scss/*.scss'), ['build-sass']);
 });
-gulp.task('watch-jade', ['build-jade'], function () {
-	gulp.watch('./views/**/*.js', ['build-jade']);
-});
+
 gulp.task('watch-server', ['server'], function () {
-	gulp.watch('./routes/**/*.js', ['server']);
-	gulp.watch(['./utils/**/*.js', '!./temp/**'], ['server']);
-	gulp.watch('./setup.js', ['server']);
-	gulp.watch('./app.js', ['server']);
+	gulp.watch(path.join(__dirname, '/routes/**/*.js'), ['server']);
+	gulp.watch([path.join(__dirname, '/utils/**/*.js')], ['server']);
+	gulp.watch(path.join(__dirname, '/setup.js'), ['server']);
+	gulp.watch(path.join(__dirname, '/app.js'), ['server']);
 });
 
-
-////// Default //////
-gulp.task('default', ['watch-sass', 'watch-server'], function(){});
+////// Tasks //////
+gulp.task('default', ['watch-sass', 'watch-server']);
