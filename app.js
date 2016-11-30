32f388d6c91cac4e8991855b703209058e131acb
David <dshuffma@us.ibm.com>
2016-11-30 17:11:16 -0500

    create marble function

--- app.js
+++ app.js
@@ -239,12 +239,14 @@ var user = users[0];
 // gogo
 check_if_already_deployed(function(not_deployed){
 	if(not_deployed){										//if this is truthy we have not yet deployed, do it now
-		deploy_chaincode(setupWebServer);
+		console.log('\nChaincode was not detected, going to deploy it now\n');
+		deploy_chaincode(create_a_marble);
 	}
 	else{													//else we already deployed
 		console.log('\nChaincode already deployed');
 		console.log('\nSetting up web server ...');
-		setupWebServer();
+		//setupWebServer();
+		create_a_marble();
 	}
 });
 
@@ -393,7 +395,61 @@ function deploy_chaincode(cb){
 			}
 		).catch(
 			function(err) {
-				console.log('Failed to end to end test with error:' + err.stack ? err.stack : err);
+				console.log('Failed, in catch block' + err.stack ? err.stack : err);
+			}
+		);
+	});
+}
+
+function create_a_marble(){
+	console.log('\nCreating a marble\n');
+	return new Promise(function(resolve, reject) {
+		console.log('Attempt to enroll: username: ' + user.username + ', secret: ' + user.secret);
+		chain.enroll('admin', 'adminpw')
+		.then(
+			function() {
+				// send proposal to endorser
+				var request = {
+					targets: [hfc.getPeer('grpc://192.168.99.100:7051'), hfc.getPeer('grpc://192.168.99.100:7056')],
+					chaincodeId : chaincode_id,
+					fcn: 'init_marble',
+					args: ['test', 'blue', '35','bob']
+				};
+				return webUser.sendTransactionProposal(request);
+			},
+			function(err) {
+				console.log('Failed to send invoke due to error: ' + err.stack ? err.stack : err);
+			}
+		).then(
+			function(results) {
+				var proposalResponses = results[0];
+				var proposal = results[1];
+				if (proposalResponses[0].response.status === 200) {
+					console.log('Successfully obtained transaction endorsement.' + JSON.stringify(proposalResponses));
+					return webUser.sendTransaction(proposalResponses, proposal);
+				} else {
+					console.log('Failed to obtain transaction endorsement. Error code: ' + proposalResponses[0].response.status);
+				}
+			},
+			function(err) {
+				console.log('Failed to send transaction proposal due to error: ' + err.stack ? err.stack : err);
+			}
+		).then(
+			function(response) {
+				if (response.Status === 'SUCCESS') {
+					console.log('Successfully ordered endorsement transaction.');
+					console.log(' need to wait now for the committer to catch up');
+					setupWebServer();
+				} else {
+					console.log('Failed to order the endorsement of the transaction. Error code: ' + response.status);
+				}
+			},
+			function(err) {
+				console.log('Failed to send transaction proposal due to error: ' + err.stack ? err.stack : err);
+			}
+		).catch(
+			function(err) {
+				console.log('Failed, in catch block' + err.stack ? err.stack : err);
 			}
 		);
 	});
