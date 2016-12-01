811df125ad13cad588483e3f766541bd041d2732
David <dshuffma@us.ibm.com>
2016-12-01 11:45:04 -0500

    move deploy code into lib

--- app.js
+++ app.js
@@ -236,7 +236,7 @@ chain.setOrderer('grpc://192.168.99.100:5151');
 var webUser;
 var chaincode_prefix = 'marbles';								//name to identify chaincode
 var chaincode_id = null;										//full name to identify chaincode
-var user = users[0];
+//var user = users[0];
 
 var marbles_lib = null;
 
@@ -269,180 +269,26 @@ make_chaincode_id(function(){
 	console.log('[0] this should only appear once, right?');
 	marbles_lib = require('./utils/marbles_cc_lib/index.js')(chain, chaincode_id, null);
 
-	check_if_already_deployed(function(not_deployed){
+	marbles_lib.check_if_already_deployed(function(not_deployed, enrollUser){
 		if(not_deployed){										//if this is truthy we have not yet deployed, do it now
-			console.log('\n[1]Chaincode was not detected, going to deploy it now\n');
-			deploy_chaincode(setup_application);
+			console.log('\n\n[1]Chaincode was not detected, going to deploy it now\n\n');
+			marbles_lib.deploy_chaincode(setup_application);
 		}
 		else{													//else we already deployed
-			console.log('\n[1]Chaincode already deployed');
+			console.log('\n\n[1]Chaincode already deployed\n\n');
 			console.log('\nSetting up web server ...');
 			setupWebServer();				//starts the webapp
-			//setup_application(); 			//builds marbles
+			//setup_application(enrollUser); 			//builds marbles
 			//marbles_lib.reset_marble_index();			//reset 
 		}
 	});
 });
-
-function check_if_already_deployed(cb){
-	return new Promise(function(resolve, reject) {
-		console.log('Attempt to enroll: username: ' + user.username + ', secret: ' + user.secret);
-		chain.enroll('admin', 'adminpw')
-		.then(
-			function(admin) {
-				console.log('Successfully enrolled user \'admin\'');
-				webUser = admin;
-
-				// send query
-				var request = {
-					targets: [hfc.getPeer('grpc://192.168.99.100:7051'), hfc.getPeer('grpc://192.168.99.100:7056')],
-					chaincodeId : chaincode_id,
-					fcn: 'read',
-					args: ['_marbleindex']
-				};
-				return webUser.queryByChaincode(request);
-			},
-			function(err) {
-				console.log('Failed to wait-- error: ' + err.stack ? err.stack : err);
-				//if(cb) return cb(err);
-				//else return;
-			}
-		).then(
-			function(response_payloads) {
-				if(response_payloads.length <= 0){
-					console.log('Query response is empty: ');
-					if(cb) return cb({error: 'query response is empty'});
-				}
-				else{
-					for(i in response_payloads) {		//pray to the gods that i===peer_id and they never move
-						console.log('Peer' + i, 'payload says:', response_payloads[i].toString('utf8'));
-					}
-					console.log('---------------------------------------------------------------------------');
-					if(cb) return cb(null);
-				}
-			},
-			function(err) {
-				console.log('Failed to send query due to error: ' + err.stack ? err.stack : err);
-				//if(cb) return cb(err);
-				//else return;
-			}
-		).catch(
-			function(err) {
-				console.log('Failed, in catch block?' + err.stack ? err.stack : err);
-				//if(cb) return cb(err);
-				//else return;
-			}
-		);
-	});
-}
-
-function deploy_chaincode(cb){
-	return new Promise(function(resolve, reject) {
-		//console.log('Attempt to enroll: username: ' + user.username + ', secret: ' + user.secret);
-		chain.enroll('admin', 'adminpw')
-		//chain.enroll(user.username, user.secret)
-		.then(
-			function(admin) {
-				console.log('Successfully enrolled user \'admin\'');
-				webUser = admin;
-
-				// send proposal to endorser
-				var request = {
-					targets: [hfc.getPeer('grpc://192.168.99.100:7051'), hfc.getPeer('grpc://192.168.99.100:7056')],
-					chaincodePath: 'local/marbles-hfc/marbles-v1/chaincode',
-					chaincodeId: chaincode_id,
-					fcn: 'init',
-					args: ['99'],
-					'dockerfile-contents' :
-					'from hyperledger/fabric-ccenv\n' +
-					'COPY . $GOPATH/src/build-chaincode/\n' +
-					'WORKDIR $GOPATH\n\n' +
-					'RUN go install build-chaincode && mv $GOPATH/bin/build-chaincode $GOPATH/bin/%s'
-				};
-
-				return admin.sendDeploymentProposal(request);
-			},
-			function(err) {
-				console.log('Failed to enroll user \'admin\'. ' + err);
-			}
-		).then(
-			function(results) {
-				var proposalResponses = results[0];
-				//console.log('proposalResponses:'+JSON.stringify(proposalResponses));
-				var proposal = results[1];
-				if (proposalResponses && proposalResponses[0].response && proposalResponses[0].response.status === 200) {
-					console.log('Successfully sent Proposal and received ProposalResponse: ');
-					console.log('\tStatus -', proposalResponses[0].response.status, 'message -', proposalResponses[0].response.message,
-						'metadata -', proposalResponses[0].response.payload, 'endorsement signature:', proposalResponses[0].endorsement.signature);
-					return webUser.sendTransaction(proposalResponses, proposal);
-				} else {
-					console.log('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-				}
-			},
-			function(err) {
-				console.log('Failed to send deployment proposal due to error: ' + err.stack ? err.stack : err);
-			}
-		).then(
-				function(response) {
-					if (response.Status === 'SUCCESS') {
-						console.log('Successfully ordered deployment endorsement.');
-						console.log(' need to wait now for the committer to catch up');
-						return sleep(20000);
-					} else {
-						console.log('Failed to order the deployment endorsement. Error code: ' + response.status);
-					}
-
-				},
-				function(err) {
-					console.log('Failed to send deployment e due to error: ' + err.stack ? err.stack : err);
-				}
-		).then(
-			function() {
-				// send query
-				var request = {
-					targets: [hfc.getPeer('grpc://192.168.99.100:7051'), hfc.getPeer('grpc://192.168.99.100:7056')],
-					chaincodeId : chaincode_id,
-					fcn: 'read',
-					args: ['_marbleindex']
-				};
-				return webUser.queryByChaincode(request);
-			},
-			function(err) {
-				console.log('Failed to wait-- error: ' + err.stack ? err.stack : err);
-			}
-		).then(
-			function(response_payloads) {
-				for(let i = 0; i < response_payloads.length; i++) {
-					console.log(response_payloads[i].toString('utf8'),'300','checking query results are correct that user b has 300 now after the move');
-				}
-
-				console.log('---------------------------------------------------------------------------');
-				console.log('\nChaincode deployed successfully');
-				console.log('\nSetting up web server ...');
-				if(cb) return cb();
-				else return;
-			},
-			function(err) {
-				console.log('Failed to send query due to error: ' + err.stack ? err.stack : err);
-			}
-		).catch(
-			function(err) {
-				console.log('Failed, in catch block' + err.stack ? err.stack : err);
-			}
-		);
-	});
-}
-
-function sleep(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 function getRandomInt(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min)) + min;
 }
-
 
 function randStr(length){
 	var text = '';
@@ -469,7 +315,8 @@ function build_marble_options(username){
 }
 
 //this only runs after we deploy
-function setup_application(){
+function setup_application(enrollUser){
+	webUser = enrollUser;
 	//var marble_users = ['bob', 'bill'];
 	var marble_users = ['bob'];
 	async.eachLimit(marble_users, 1, function(username, user_cb) {			//iter through each one
--- utils/marbles_cc_lib/index.js
+++ utils/marbles_cc_lib/index.js
@@ -3,13 +3,23 @@
 //var url = require('url');
 
 //-------------------------------------------------------------------
-// Marbles Chaincode Library	
+// Marbles Chaincode Library
 //-------------------------------------------------------------------
 module.exports = function (chain, chaincode_id, logger) {
-	//var deploy_cc = require('./deploy_cc.js')(chain, chaincode_id, logger);
+	var deploy_cc = require('./deploy_cc.js')(chain, chaincode_id, logger);
 	var marbles = require('./marbles.js')(chain, chaincode_id, logger);
 	//var users = require('./users.js')(chain, chaincode_id, logger);
 	var marbles_chaincode = {};
+
+	//deploy chaincode
+	marbles_chaincode.deploy_chaincode = function (cb) {
+		deploy_cc.deploy_chaincodefunction(cb);
+	};
+
+	//deploy chaincode
+	marbles_chaincode.check_if_already_deployed = function (cb) {
+		deploy_cc.check_if_already_deployed(cb);
+	};
 
 	//create a marble
 	marbles_chaincode.create_a_marble = function (webUser, options, cb) {
@@ -20,7 +30,6 @@ module.exports = function (chain, chaincode_id, logger) {
 	marbles_chaincode.reset_marble_index = function (webUser, cb) {
 		marbles.reset_marble_index(webUser, cb);
 	};
-
 
 	return marbles_chaincode;
 };
