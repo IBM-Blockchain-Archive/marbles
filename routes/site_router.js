8bf408e89022bd82421b4b6ce06f8d826ca38d7d
David <dshuffma@us.ibm.com>
2016-12-07 11:12:47 -0500

    added login

--- routes/site_router.js
+++ routes/site_router.js
@@ -13,8 +13,9 @@ var router = express.Router();
 var setup = require('../setup.js');
 
 //anything in here gets passed to JADE template engine
-function build_bag(){
+function build_bag(req){
 	return {
+				session: req.session.user,					//pass session info
 				setup: setup,								//static vars for configuration settings
 				e: process.error,							//send any setup errors
 				jshash: process.env.cachebust_js,			//js cache busting hash (not important)
@@ -31,20 +32,43 @@ router.route('/').get(function(req, res){
 });
 
 // ============================================================================================================================
+// Login
+// ============================================================================================================================
+router.route('/login').get(function(req, res){
+	res.render('login', {title: 'Marbles - Login', bag: build_bag(req)});
+});
+
+router.route('/login').post(function(req, res){
+	req.session.user = {username: req.body.username};
+	res.redirect('/home');
+});
+
+router.route('/logout').get(function(req, res){
+	req.session.destroy();
+	res.redirect('/login');
+});
+
+
+// ============================================================================================================================
 // Home
 // ============================================================================================================================
 router.route('/home').get(function(req, res){
-	res.render('marbles', {title: 'Marbles - Home', bag: build_bag()});
+	if(!req.session.user || !req.session.user.username){
+		res.redirect('/login');
+	}
+	else{
+		res.render('marbles', {title: 'Marbles - Home', bag: build_bag(req)});
+	}
 });
 
 // ============================================================================================================================
 // Part 2
 // ============================================================================================================================
 router.route('/p2').get(function(req, res){
-	res.render('part2', {title: 'Marbles Part 2', bag: build_bag()});
+	res.render('part2', {title: 'Marbles Part 2', bag: build_bag(req)});
 });
 router.route('/p2/:page?').get(function(req, res){
-	res.render('part2', {title: 'Marbles Part 2', bag: build_bag()});
+	res.render('part2', {title: 'Marbles Part 2', bag: build_bag(req)});
 });
 
 module.exports = router;
