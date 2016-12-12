b6736adfec3bf1404c170233eba687fbdd8a7e1d
David <dshuffma@us.ibm.com>
2016-12-12 13:11:43 -0500

    initial fancy tx story done

--- public/js/tx_story.js
+++ public/js/tx_story.js
@@ -43,18 +43,20 @@ function show_tx_step(obj){
 		story2_animation();
 	}
 	else if(state === 'ordering'){
-		$('#txStep1, #txStep2, #txStep3').removeClass('inactiveStep');
-		$('#txStep4').addClass('inactiveStep');
-		$('#txStep1, #txStep2').addClass('stepComplete');
+		setTimeout(function(){
+			$('#txStep1, #txStep2, #txStep3').removeClass('inactiveStep');
+			$('#txStep4').addClass('inactiveStep');
+			$('#txStep1, #txStep2').addClass('stepComplete');
 
-		story3_animation();
-		setTimeout(function(){show_tx_step({state: 'committing'});}, 3000);
+			story3_animation();
+			setTimeout(function(){show_tx_step({state: 'committing'});}, 4000);
+		}, 1000);
 	}
 	else if(state === 'committing'){
 		$('#txStep1, #txStep2, #txStep3, #txStep4').removeClass('inactiveStep');
 		$('#txStep1, #txStep2, #txStep3').addClass('stepComplete');
 
-		story4_animation(200);
+		story4_animation(220);
 		setTimeout(function(){show_tx_step({state: 'finished'});}, 5000);
 	}
 	else if(state === 'finished'){
@@ -64,6 +66,7 @@ function show_tx_step(obj){
 }
 
 
+//1. animate borders to join marble in center
 function story1_animation(){
 	var dist = 50;
 	$('#marbleBorderTop, #marbleBorderBottom, #marbleBorderLeft, #marbleBorderRight').show();
@@ -73,7 +76,11 @@ function story1_animation(){
 	$('#marbleBorderRight').animate({left: '-=' + dist}, {duration: 800});
 }
 
-
+//1. show marble that will roll 
+//2. hide all other border marbles (?) dsh to do remove
+//3. roll it
+//4. hide rolled marble
+//5. show endorse marble with icon
 function story2_animation(){
 	$('#proposeMarble').show();
 
@@ -83,21 +90,45 @@ function story2_animation(){
 	$('#marbleBorderLeft').hide();
 	$('#marbleBorderRight').hide();
 
-	roll_ball('#proposeMarbleStable', 220, function(){
+	roll_ball('#proposeMarble', 235, function(){
+		$('#proposeMarble').hide();
 		$('#endorseMarbleStable').show();
 	});
 }
 
+//1. show the marble that will roll
+//2. roll endorsed marble
+//3. show orderer marbles
+//4. build box around marbles
 function story3_animation(){
 	$('#endorseMarble').show();
-	roll_ball('#endorseMarble', 250, function(){
+	roll_ball('#endorseMarble', 240, function(){
 		$('#endorseMarbleStable').show();
-		$('.ordererMarbles:first').css('left', '420px').fadeIn();
-		$('.ordererMarbles:last').css('left', '370px').fadeIn();
-		$('#orderBox').fadeIn();
+		$('.ordererMarbles').fadeIn();
+		setTimeout(function(){
+			$('#orderBoxStable').fadeIn(1000);
+		}, 300);
 	});
 }
 
+//1. fade in solid box around marbles
+//2. animate it right
+function story4_animation(dist){
+	$('#orderBox').fadeIn(1000);
+	setTimeout(function(){
+		//$('#proposeMarbleStable, .ordererMarbles').hide();
+		setTimeout(function(){
+			$('#orderBox').animate({left: '+=' + dist}, {
+				duration: 2000,
+				/*complete: function(){
+					$('#orderBoxStable').css('background', '#2EB9D6');
+				}*/
+			});
+		}, 500);
+	}, 1000);
+}
+
+//roll a circle right xxx distance
 function roll_ball(who, move, cb) {
 	if(!cb) cb = function(){};
 
@@ -110,19 +141,3 @@ function roll_ball(who, move, cb) {
 		complete: cb
 	});
 }
-
-function story4_animation(dist){
-	$('#orderBox').css('background', '#2EB9D6');
-	setTimeout(function(){
-		$('#proposeMarbleStable, .ordererMarbles').hide();
-		setTimeout(function(){
-			$('#orderBox').animate({left: '+=' + dist}, {
-				duration: 2000,
-				/*complete: function(){
-					$('#orderBox').css('background', '#2EB9D6');
-				}*/
-			});
-		}, 500);
-	}, 500);
-}
-
