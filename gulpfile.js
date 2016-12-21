748e72f21f081091f2fea0b6d3099316de79be00
David <dshuffma@us.ibm.com>
2016-12-21 15:25:20 -0500

    LINT

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-///// Gulp Dependencies /////
 var path = require('path');
 var gulp = require('gulp');
 var	sass = require('gulp-sass');
@@ -7,10 +6,9 @@ var cleanCSS = require('gulp-clean-css');
 var rename = require('gulp-rename');
 var bust = require('gulp-buster');
 var spawn = require('child_process').spawn;
-var fs = require('fs');
 var node, env = process.env;
 
-////// Build Tasks ///////
+// ---------------- Build Stuff Tasks ---------------- //
 gulp.task('build-sass', function () {
 	gulp.src(path.join(__dirname, '/scss/*.scss'))
 		.pipe(sass().on('error', sass.logError))
@@ -32,13 +30,15 @@ gulp.task('build-js-hash', function () {
 		.pipe(gulp.dest('.'));										//dump busters_js.json
 });
 
-////// Run Server Task ///////
+
+// ---------------- Run Application Task ---------------- //
 gulp.task('server', function() {
 	if(node) node.kill();
-	node = spawn('node', ['app.js'], {env: env, stdio: 'inherit'});			//command, file, options
+	node = spawn('node', ['app.js'], {env: env, stdio: 'inherit'});	//command, file, options
 });
 
-////// Watch Tasks //////
+
+// ---------------- Watch for Changes Tasks ---------------- //
 gulp.task('watch-sass', ['build-sass'], function () {
 	gulp.watch(path.join(__dirname, '/scss/*.scss'), ['build-sass']);
 });
@@ -54,7 +54,8 @@ gulp.task('watch-server', ['server'], function () {
 	gulp.watch(path.join(__dirname, '/app.js'), ['server']);
 });
 
-////// Tasks //////
+
+// ---------------- Gulp Tasks ---------------- //
 gulp.task('default', ['watch-js', 'watch-sass', 'watch-server']);
 gulp.task('marbles', ['start_marbles', 'default']);
 gulp.task('united_marbles', ['start_mtc1', 'default']);
@@ -67,19 +68,19 @@ gulp.task('start_marbles', function () {
 	console.log('\n[International Marbles Trading Consortium]\n');
 });
 
-// MTC Member 1
+// IMTC Member 1
 gulp.task('start_mtc1', function () {
 	console.log('\n[International Marbles Trading Consortium] - Member "United Marbles"\n');
 	env['creds_filename'] = 'creds_united_marbles.json';
 });
 
-// MTC Member 2
+// IMTC Member 2
 gulp.task('start_mtc2', function () {
 	console.log('\n[International Marbles Trading Consortium] - Member "Marble Market"\n');
 	env['creds_filename'] = 'creds_marble_market.json';
 });
 
-// MTC Member 3
+// IMTC Member 3
 gulp.task('start_mtc3', function () {
 	console.log('\n[International Marbles Trading Consortium] - Member "eMarbles"\n');
 	env['creds_filename'] = 'creds_emarbles.json';
