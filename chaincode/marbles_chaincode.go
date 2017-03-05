10a015b43a32ae124d841237a36b06b532eb99f9
qianlizimu <qianlizimu@aliyun.com>
2017-03-05 14:11:01 +0800

    Update marbles_chaincode.go

--- chaincode/marbles_chaincode.go
+++ chaincode/marbles_chaincode.go
@@ -298,7 +298,7 @@ func (t *SimpleChaincode) init_marble(stub shim.ChaincodeStubInterface, args []s
 	}
 	
 	//build the marble json string manually
-	str := `{"name": "` + name + `", "color": "` + color + `", "size": ` + strconv.Itoa(size) + `, "user": "` + user + '","feature":"'+feature+`"}`
+	str := `{"name": "` + name + `", "color": "` + color + `", "size": ` + strconv.Itoa(size) + ', "user":"'+ user + '","feature":"'+feature+'"}'
 	err = stub.PutState(name, []byte(str))									//store marble with id as key
 	if err != nil {
 		return nil, err
