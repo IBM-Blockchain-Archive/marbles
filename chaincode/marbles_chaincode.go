9251c2a38e0d58e562992d90e40c14c4ff550b00
qianlizimu <qianlizimu@aliyun.com>
2017-03-06 09:34:53 +0800

    Update marbles_chaincode.go

--- chaincode/marbles_chaincode.go
+++ chaincode/marbles_chaincode.go
@@ -280,7 +280,7 @@ func (t *SimpleChaincode) init_marble(stub shim.ChaincodeStubInterface, args []s
 	color := strings.ToLower(args[1])
 	user := strings.ToLower(args[3])
 	size, err := strconv.Atoi(args[2])
-	feature=args[4]
+	feature:=args[4]
 	if err != nil {
 		return nil, errors.New("3rd argument must be a numeric string")
 	}
