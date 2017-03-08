d0d0ca37eda4b808008db707672cd4b600290ecc
qianlizimu <qianlizimu@aliyun.com>
2017-03-08 22:53:57 +0800

    change from marble to user

--- chaincode/marbles_chaincode.go
+++ chaincode/marbles_chaincode.go
@@ -1,21 +1,3 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
 
 package main
 
@@ -24,7 +6,6 @@ import (
 	"fmt"
 	"strconv"
 	"encoding/json"
-	"time"
 	
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
@@ -48,16 +29,7 @@ type Description struct{
 	Size int `json:"size"`
 }
 
-type AnOpenTrade struct{
-	User string `json:"user"`					//user who created the open trade order
-	Timestamp int64 `json:"timestamp"`			//utc timestamp of creation
-	Want Description  `json:"want"`				//description of desired marble
-	Willing []Description `json:"willing"`		//array of marbles willing to trade away
-}
 
-type AllTrades struct{
-	OpenTrades []AnOpenTrade `json:"open_trades"`
-}
 
 // ============================================================================================================================
 // Main
@@ -128,7 +100,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 		return t.Init(stub, "init", args)
 	} else if function == "delete" {										//deletes an entity from its state
 		res, err := t.Delete(stub, args)
-		cleanTrades(stub)													//lets make sure all open trades are still valid
+															//lets make sure all open trades are still valid
 		return res, err
 	} else if function == "write" {											//writes a value to the chaincode state
 		return t.Write(stub, args)
@@ -302,4 +274,3 @@ func (t *SimpleChaincode) create_user(stub shim.ChaincodeStubInterface, args []s
 	fmt.Println("- end init marble")
 	return nil, nil
 }
-
