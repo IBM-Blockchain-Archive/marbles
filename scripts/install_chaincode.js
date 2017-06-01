1b50a08c7a45ac9dd64be7c8dd4bb6defde88c5d
David <dshuffma@us.ibm.com>
2017-06-01 18:03:24 -0400

    fix helper.js when no tls

--- scripts/install_chaincode.js
+++ scripts/install_chaincode.js
@@ -8,7 +8,7 @@ var logger = new (winston.Logger)({
 		new (winston.transports.Console)({ colorize: true }),
 	]
 });
-var helper = require(path.join(__dirname, '../utils/helper.js'))('marbles1.json', logger);			//set the config file name here
+var helper = require(path.join(__dirname, '../utils/helper.js'))('marbles3.json', logger);			//set the config file name here
 var fcw = require(path.join(__dirname, '../utils/fc_wrangler/index.js'))({ block_delay: helper.getBlockDelay() }, logger);
 
 console.log('---------------------------------------');
@@ -33,7 +33,7 @@ fcw.enroll(helper.makeEnrollmentOptions(0), function (enrollErr, enrollResp) {
 		};
 		fcw.install_chaincode(enrollResp, opts, function (err, resp) {
 			console.log('---------------------------------------');
-			logger.info('Install done. Errors:', err);
+			logger.info('Install done. Errors:', (err) ? 'nope' : err);
 			console.log('---------------------------------------');
 		});
 	}
