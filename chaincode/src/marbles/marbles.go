871a6cf68386f0f2248116e2ebceb68eec283168
David <dshuffma@us.ibm.com>
2017-08-03 18:34:14 -0400

    fix upgrade woes with chaincode in init()

--- chaincode/src/marbles/marbles.go
+++ chaincode/src/marbles/marbles.go
@@ -71,34 +71,55 @@ func main() {
 
 
 // ============================================================================================================================
-// Init - initialize the chaincode - marbles don’t need anything initlization, so let's run a dead simple test instead
+// Init - initialize the chaincode 
+//
+// Marbles does not require initialization, so let's run a simple test instead.
+//
+// Shows off PutState() and how to pass an input argument to chaincode.
+//
+// Inputs - Array of strings
+//  ["314"]
+// 
+// Returns - shim.Success or error
 // ============================================================================================================================
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("Marbles Is Starting Up")
 	_, args := stub.GetFunctionAndParameters()
 	var Aval int
 	var err error
+	
+	fmt.Println("Init() args count:", len(args))
+	fmt.Println("Init() args found:", args)
 
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
+	// expecting 1 arg for instantiate or upgrade
+	if len(args) == 1 {
+		fmt.Println("Init() arg[0] length", len(args[0]))
 
-	// convert numeric string to integer
-	Aval, err = strconv.Atoi(args[0])
-	if err != nil {
-		return shim.Error("Expecting a numeric string argument to Init()")
+		// expecting arg[0] to be length 0 for upgrade
+		if len(args[0]) == 0 {
+			fmt.Println("args[0] is empty... must be upgrading")
+		} else {
+			fmt.Println("args[0] is not empty, must be instantiating")
+
+			// convert numeric string to integer
+			Aval, err = strconv.Atoi(args[0])
+			if err != nil {
+				return shim.Error("Expecting a numeric string argument to Init() for instantiate")
+			}
+
+			// this is a very simple test. let's write to the ledger and error out on any errors
+			// it's handy to read this right away to verify network is healthy if it wrote the correct value
+			err = stub.PutState("selftest", []byte(strconv.Itoa(Aval)))
+			if err != nil {
+				return shim.Error(err.Error())                  //self-test fail
+			}
+		}
 	}
 
 	// store compaitible marbles application version
 	err = stub.PutState("marbles_ui", []byte("4.0.0"))
 	if err != nil {
 		return shim.Error(err.Error())
-	}
-
-	// this is a very simple dumb test.  let's write to the ledger and error on any errors
-	err = stub.PutState("selftest", []byte(strconv.Itoa(Aval))) //making a test var "selftest", its handy to read this right away to test the network
-	if err != nil {
-		return shim.Error(err.Error())                          //self-test fail
 	}
 
 	fmt.Println(" - ready for action")                          //self-test pass
